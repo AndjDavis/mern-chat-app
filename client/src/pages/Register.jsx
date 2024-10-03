33ffeea907708752807e8c031538e103c3846aac
AndjDavis <andi.idavis@gmail.com>
2024-10-03 16:28:40 -0600

    set up basic register component with styled-component

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,3 +1,127 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import Logo from "../assets/logo.svg";
+
 export default function Register() {
-	return <h1>Register</h1>;
+	const handleChange = (e) => {
+		console.log("change", e);
+	};
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		alert("Form");
+		console.log("Event", e);
+	};
+	return (
+		<>
+			<FormContainer>
+				<form onSubmit={handleSubmit}>
+					<div className="brand">
+						<img
+							src={Logo}
+							alt="logo"
+						/>
+						<h1>snappy</h1>
+					</div>
+					<input
+						type="text"
+						placeholder="Username"
+						name="username"
+						onChange={handleChange}
+					/>
+					<input
+						type="email"
+						placeholder="Email"
+						name="email"
+						onChange={handleChange}
+					/>
+					<input
+						type="password"
+						placeholder="Password"
+						name="Password"
+						onChange={handleChange}
+					/>
+					<input
+						type="password"
+						placeholder="Confirm Password"
+						name="confirmPassword"
+						onChange={handleChange}
+					/>
+					<button type="submit">Create New User</button>
+					<span>
+						Already Have an account?
+						<Link to="/login"> Login </Link>
+					</span>
+				</form>
+			</FormContainer>
+		</>
+	);
 }
+
+const FormContainer = styled.div`
+	height: 100vh;
+	width: 100vw;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	gap: 1rem;
+	align-items: center;
+	background-color: #131324;
+	.brand {
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+		justify-content: center;
+		img {
+			height: 5rem;
+		}
+		h1 {
+			color: white;
+			text-transform: uppercase;
+		}
+	}
+
+	form {
+		display: flex;
+		flex-direction: column;
+		gap: 2rem;
+		background-color: #00000076;
+		border-radius: 2rem;
+		padding: 3rem 5rem;
+	}
+	input {
+		background-color: transparent;
+		padding: 1rem;
+		border: 0.1rem solid #4e0eff;
+		border-radius: 0.4rem;
+		color: white;
+		width: 100%;
+		font-size: 1rem;
+		&:focus {
+			border: 0.1rem solid #997af0;
+			outline: none;
+		}
+	}
+	button {
+		background-color: #4e0eff;
+		color: white;
+		padding: 1rem 2rem;
+		border: none;
+		font-weight: bold;
+		cursor: pointer;
+		border-radius: 0.4rem;
+		font-size: 1rem;
+		text-transform: uppercase;
+		&:hover {
+			background-color: #4e0eff;
+		}
+	}
+	span {
+		color: white;
+		text-transform: uppercase;
+		a {
+			color: #4e0eff;
+			text-decoration: none;
+			font-weight: bold;
+		}
+	}
+`;
