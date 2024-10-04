344dbd7b46e5ac6c755da4e7cc635122682e42a4
AndjDavis <andi.idavis@gmail.com>
2024-10-03 18:25:44 -0600

    set up basic users api

--- server/index.js
+++ server/index.js
@@ -3,15 +3,16 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const connectDB = require("./config/db");
+const userRoutes = require("./routes/users/index");
 
 require("dotenv").config();
 
 const app = express();
-
-connectDB();
-
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", userRoutes);
+
+connectDB();
 
 const PORT = process.env.PORT;
 const server = app.listen(PORT, () => {
