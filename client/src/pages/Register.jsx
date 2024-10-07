b2af3d6e41659b0d5a24b29e64b772a30481032c
AndjDavis <andi.idavis@gmail.com>
2024-10-07 11:15:29 -0600

    registerNewUser talking and creating new user on backend.

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,25 +1,26 @@
 import { useState } from "react";
-import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import Logo from "../assets/logo.svg";
-import routes from "../utils/routes";
 import { toastOptions } from "../utils/constants";
+import { registerNewUser } from "../services/authService";
 
 const initialState = {
-	username: "",
-	email: "",
-	password: "",
-	confirmPassword: "",
+	username: "test user",
+	email: "example@example.com",
+	password: "testtest",
+	confirmPassword: "testtest",
 };
 const usernameMinLength = 3;
 const pwMinLength = 8;
 
 export default function Register() {
+	const navigate = useNavigate();
 	const [values, setValues] = useState(initialState);
+
 	const handleChange = (e) => {
 		setValues((currentValues) => ({
 			...currentValues,
@@ -29,9 +30,10 @@ export default function Register() {
 
 	const handleValidation = () => {
 		const { confirmPassword, password } = values;
+
 		let toastMessage = null;
 		if (password !== confirmPassword) {
-			toastMessage = "Your passwords don't match!";
+			toastMessage = "Passwords do not match!";
 		}
 
 		if (!toastMessage) return true;
@@ -39,23 +41,31 @@ export default function Register() {
 		return false;
 	};
 
-	const handleRequest = async () => {
-		const { email, password, username } = values;
-		const { data } = await axios.post(routes.registerRoute, {
-			username,
-			email,
-			password,
-		});
-		console.log("Register Data", data);
-	};
-
-	const handleSubmit = (e) => {
+	const handleSubmit = async (e) => {
 		e.preventDefault();
 
 		const isValid = handleValidation();
 		if (!isValid) return;
 
-		handleRequest();
+		try {
+			const { confirmPassword, ...formData } = values;
+			const { data, status } = await registerNewUser(formData);
+			if (status === 201 && data.status === "success") {
+				localStorage.setItem(
+					process.env.REACT_APP_LOCALHOST_KEY,
+					JSON.stringify(data.user)
+				);
+				navigate("/");
+			} else if (status === 409) {
+				toast.error("Email or username already exists", toastOptions);
+			} else {
+				const failMessage = data?.message || "Something went wrong...";
+				toast.error(failMessage, toastOptions);
+			}
+		} catch (error) {
+			console.error("Register Form Error: ", error);
+			toast.error("An error occurred during registration", toastOptions);
+		}
 	};
 
 	return (
