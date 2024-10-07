9d0eba7bfe328d9f322dd037f9f1845a6e5ea2ea
AndjDavis <andi.idavis@gmail.com>
2024-10-07 14:33:45 -0600

    initial login form and route

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,3 +1,106 @@
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+
+import FormLogo from "../components/FormLogo";
+import {
+	StyledButton,
+	StyledContainer,
+	StyledForm,
+	StyledInput,
+	StyledSpan,
+} from "../styles/form-styles";
+import { loginUser } from "../services/authService";
+import { toastOptions } from "../utils/constants";
+import "react-toastify/dist/ReactToastify.css";
+
+const initialState = {
+	username: "",
+	password: "",
+};
+
 export default function Login() {
-	return <h1>Login</h1>;
+	const navigate = useNavigate();
+	const [values, setValues] = useState(initialState);
+
+	const handleChange = (e) => {
+		setValues((currentValues) => ({
+			...currentValues,
+			[e.target.name]: e.target.value,
+		}));
+	};
+
+	const validateForm = () => {
+		const { username, password } = values;
+		let message = null;
+		if (username === "") {
+			message = "Email and Password is required.";
+		} else if (password === "") {
+			message = "Email and Password is required.";
+		}
+
+		if (!message) return true;
+
+		toast.error(message, toastOptions);
+		return false;
+	};
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+
+		const isValid = validateForm();
+		if (!isValid) return;
+
+		try {
+			const formData = { ...values };
+			const { data, status } = await loginUser(formData);
+			if (data?.success && status === 201) {
+				localStorage.setItem(
+					process.env.REACT_APP_LOCALHOST_KEY,
+					JSON.stringify(data.user)
+				);
+
+				navigate("/");
+			} else if (status === 409) {
+				toast.error(data.msg, toastOptions);
+			} else {
+				const failMessage = data?.message || "Something went wrong...";
+				toast.error(failMessage, toastOptions);
+			}
+		} catch (error) {
+			console.log("Login Form Error: ", error);
+			toast.error("An error occurred during login", toastOptions);
+		}
+	};
+
+	return (
+		<>
+			<StyledContainer>
+				<StyledForm onSubmit={handleSubmit}>
+					<FormLogo />
+					<StyledInput
+						type="text"
+						required
+						placeholder="Username"
+						name="username"
+						onChange={handleChange}
+						value={values.username}
+					/>
+					<StyledInput
+						type="password"
+						required
+						placeholder="Password"
+						name="password"
+						onChange={handleChange}
+						value={values.password}
+					/>
+					<StyledButton type="submit">Log In</StyledButton>
+					<StyledSpan>
+						Don't have an account ? <Link to="/register">Create One.</Link>
+					</StyledSpan>
+				</StyledForm>
+			</StyledContainer>
+			<ToastContainer />
+		</>
+	);
 }
