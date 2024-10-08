24b3a5f1e6ea6d69abe70d452c9f7419b2e916f2
AndjDavis <andi.idavis@gmail.com>
2024-10-08 15:57:23 -0600

    added Logout to Welcome screen and passed in User as param

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -2,11 +2,17 @@ import React from "react";
 import styled from "styled-components";
 import Robot from "../assets/robot.gif";
 
+import Logout from "./Logout";
 import { FlexBox } from "../styles/styles";
 
-export default function Welcome({ username = "" }) {
+export default function Welcome({ user = {} }) {
+	const username = user?.username || "User";
+
 	return (
 		<Container>
+			<div className="logout">
+				<Logout user={user} />
+			</div>
 			<img
 				src={Robot}
 				alt="robot"
@@ -23,11 +29,22 @@ const Container = styled(FlexBox)`
 	color: white;
 	img {
 		height: 20rem;
+		flex-grow: 8;
 	}
 	span {
 		color: #4e0eff;
+		flex-grow: 8;
 	}
 	h3 {
 		margin-top: 1rem;
+		flex-grow: 8;
+	}
+	.logout {
+		display: flex;
+		flex-grow: 1;
+		width: 100%;
+		justify-content: flex-end;
+		align-items: center;
+		padding-right: 0.5rem;
 	}
 `;
