3ef6974a4d61ce52947a6579836bf7890707fccf
AndjDavis <andi.idavis@gmail.com>
2024-10-07 22:42:11 -0600

    new Profile page fetching avatars from api

---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,108 @@
+import React, { useState, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import { toastOptions } from "../utils/constants";
+import { Container, SubmitButton, Title } from "../styles/styles";
+import AvatarList from "../components/AvatarList";
+import Loader from "../components/Loader";
+import useRedirectIfNotLoggedIn from "../hooks/useRedirectIfNotLoggedIn";
+import { setProfileAvatar, fetchAvatarsList } from "../services/avatarService";
+
+export default function Profile() {
+	useRedirectIfNotLoggedIn();
+	const navigate = useNavigate();
+	const [avatars, setAvatars] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [selectedAvatarIndex, setSelectedAvatarIndex] = useState(undefined);
+	const setProfilePicture = async () => {
+		if (selectedAvatarIndex === undefined) {
+			toast.error("Please select an avatar...", toastOptions);
+			return;
+		}
+
+		const { user } = await JSON.parse(
+			localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+		);
+
+		if (!user) {
+			toast.error(
+				"Couldn't find user information, please try again",
+				toastOptions
+			);
+			return;
+		}
+
+		const selectedAvatar = avatars[selectedAvatarIndex];
+		if (!selectedAvatar) {
+			toast.error("Something went wrong in the avatar selection", toastOptions);
+			return;
+		}
+
+		try {
+			const { data, status } = await setProfileAvatar(selectedAvatar, user._id);
+			if (data.isSet) {
+				user.avatarImage = data.image;
+				localStorage.setItem(
+					process.env.REACT_APP_LOCALHOST_KEY,
+					JSON.stringify(user)
+				);
+				navigate("/");
+			} else {
+				toast.error("Error setting avatar. Please try again.", toastOptions);
+			}
+		} catch (error) {
+			console.log("Register Form Error: ", error);
+			toast.error(
+				"An server error occurred while setting the avatar",
+				toastOptions
+			);
+		}
+	};
+
+	const hasFetched = useRef(false);
+
+	useEffect(() => {
+		if (hasFetched.current) return;
+		hasFetched.current = true;
+
+		const fetchAvatarData = async () => {
+			try {
+				const data = await fetchAvatarsList();
+				setAvatars(data);
+			} catch (error) {
+				console.log("Fetching Avatar Pictures Error: ", error);
+				setAvatars([]);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+		setTimeout(() => fetchAvatarData(), 200);
+	}, []);
+
+	if (isLoading) {
+		return (
+			<Container>
+				<Loader />
+			</Container>
+		);
+	}
+	return (
+		<Container>
+			<Title>
+				<h1>Pick an Avatar as your profile picture</h1>
+			</Title>
+			<AvatarList
+				avatarList={avatars}
+				selectedAvatarIndex={selectedAvatarIndex}
+				onClick={setSelectedAvatarIndex}
+			/>
+			<SubmitButton onClick={setProfilePicture}>
+				Set as Profile Picture
+			</SubmitButton>
+			<ToastContainer />
+		</Container>
+	);
+}
