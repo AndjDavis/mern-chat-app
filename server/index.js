16d2f37ba06e834630a4eef4c4843eee6fc7b2ed
AndjDavis <andi.idavis@gmail.com>
2024-10-08 15:55:44 -0600

    added io to server

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ const authRoutes = require("./routes/auth");
 const messageRoutes = require("./routes/messages");
 
 const app = express();
+const socket = require("socket.io");
 
 const FRONTEND_BASE_URL = process.env.FRONTEND_BASE_URL;
 const corOptions = {
@@ -32,8 +33,33 @@ app.use((req, res, next) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/messages", messageRoutes);
+app.get("/ping", (_req, res) => {
+	return res.json({ message: "Ping Successfull" });
+});
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
 	console.info(`Server listening on port ${PORT}`);
+});
+
+const io = socket(server, {
+	cors: {
+		origin: FRONTEND_BASE_URL,
+		credentials: true,
+	},
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+	global.chatSocket = socket;
+	socket.on("add-user", (userId) => {
+		onlineUsers.set(userId, socket.id);
+	});
+
+	socket.on("send-msg", (data) => {
+		const sendUserSocket = onlineUsers.get(data.to);
+		if (sendUserSocket) {
+			socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+		}
+	});
 });
