bc32c312e45acd2f0f1717813e929c1169d7846c
AndjDavis <andi.idavis@gmail.com>
2024-10-08 11:57:03 -0600

    Added Welcome component

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,3 +1,33 @@
-export default function Welcome() {
-	return <h1>Welcome</h1>;
+import React from "react";
+import styled from "styled-components";
+import Robot from "../assets/robot.gif";
+
+import { FlexBox } from "../styles/styles";
+
+export default function Welcome({ username = "" }) {
+	return (
+		<Container>
+			<img
+				src={Robot}
+				alt="robot"
+			/>
+			<h1>
+				Welcome, <span>{username}!</span>
+			</h1>
+			<h3>Please select a chat to Start messaging.</h3>
+		</Container>
+	);
 }
+
+const Container = styled(FlexBox)`
+	color: white;
+	img {
+		height: 20rem;
+	}
+	span {
+		color: #4e0eff;
+	}
+	h3 {
+		margin-top: 1rem;
+	}
+`;
